97d005b1829e354d069a49c31847602f4e5f1078
Kira <90365542+Kira272921@users.noreply.github.com>
2022-03-13 17:17:14 +0530

    feat: ✨readme script (#5)
    * feat: :sparkles: placeholder support for token snippets
    
    * feat: :technologist: add readme script
    
    * feat: :technologist: add husky pre commit git hook
    
    * docs: :memo: generate README.md
    
    * :memo: update README.md
    
    * fix: 🐛 fix bugs in the husky git hook
    
    Co-authored-by: Avneesh Agarwal <avneeshagarwal0612@gmail.com>
    
    * docs: update README.md📝
    
    Co-authored-by: Avneesh Agarwal <avneeshagarwal0612@gmail.com>

--- scripts/readme.js
+++
@@ -1,7 +0,0 @@
-const nftCollectionSnippets = require("../snippets/nft-collection.json");
-const tokenSnippets = require("../snippets/token.json");
-
-var totalSnippets =
-  Object.keys(nftCollectionSnippets).length + Object.keys(tokenSnippets).length;
-
-console.log(totalSnippets);
---
+++ utils/convertFileName.js
@@ -0,0 +1,15 @@
+function convertFileName(fileName) {
+  for (const regexPattern of [/\..+$/]) {
+    var fileSlug = fileName.replace(regexPattern, "");
+  }
+  var words = fileSlug.split("-");
+
+  for (var i = 0; i < words.length; i++) {
+    var word = words[i];
+    words[i] = word.charAt(0).toUpperCase() + word.slice(1);
+  }
+
+  return words.join(" ");
+}
+
+export default convertFileName;
