d9d36445559299fe61c8ada39e511d63716fde78
Anurag <kr.anurag24@gmail.com>
2022-03-13 13:27:33 +0000

    feat: 🎉 init nextjs with chakra ui

---
+++ web/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+
+import { Box } from '@chakra-ui/react'
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <Box>
+    Hello
+    </Box>
+    </>
+  )
+}
+
+export default Home
