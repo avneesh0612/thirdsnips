6fee031536952bfeae70821c7b5cff9ac6b33aa0
Anurag <kr.anurag24@gmail.com>
2022-03-13 14:12:05 +0530

    feat: ✨ add vote collection snippets (#4)
    * ✨ feat: add module collection snippets
    
    * feat: ✨ add vote collection snippets
    
    Co-authored-by: Avneesh Agarwal <avneeshagarwal0612@gmail.com>

--- scripts/readme.js
+++ scripts/readme.js
@@ -1,6 +1,7 @@
-const nftCollectionSnippets = require('../snippets/nft-collection.json')
-const tokenSnippets = require('../snippets/token.json')
+const nftCollectionSnippets = require("../snippets/nft-collection.json");
+const tokenSnippets = require("../snippets/token.json");
 
-var totalSnippets = Object.keys(nftCollectionSnippets).length + Object.keys(tokenSnippets).length
+var totalSnippets =
+  Object.keys(nftCollectionSnippets).length + Object.keys(tokenSnippets).length;
 
-console.log(totalSnippets)
+console.log(totalSnippets);
