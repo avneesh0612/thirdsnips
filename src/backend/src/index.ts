05df9be81533cf6503d28044d1cc03931158930a
Kira <90365542+Kira272921@users.noreply.github.com>
2022-03-22 06:42:39 +0530

    ✨ feat: add auth0 to the backend (#11)
    * feat: :construction: base version of the NFT mint feature
    
    * :bricks: chore: made the repo into a monorepo
    
    * :sparkles: feat: base version of the webview
    
    * :sparkles: feat: add auth0 to the backend
    
    * feat: add github sign in button and format files
    
    * feat: :fire: using username and password for the auth0 login
    
    * feat: :sparkles: add side panel and complete the auth stuff
    
    Co-authored-by: Avneesh Agarwal <avneeshagarwal0612@gmail.com>

---
+++ src/backend/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { auth } from "express-openid-connect";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config({ path: ".env" });
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+app.use(auth(config));
+
+app.get("/", (_req, res) => {
+  res.send("Hello World!");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port} in ${process.env.MODE} mode`);
+});
