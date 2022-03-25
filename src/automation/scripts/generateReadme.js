65d241804709b1a6ba2eea4f924bb765f426e5f2
Kira272921 <sunset272921@gmail.com>
2022-03-25 22:06:27 +0530

    :bulb: chore: add comments in automation scripts

--- src/automation/scripts/generateReadme.js
+++ src/automation/scripts/generateReadme.js
@@ -4,6 +4,7 @@ const markdownTable = require("markdown-table");
 const { readmeTop, readmeBottom } = require("../data/readmeConstants");
 const constants = require("../data/constants");
 
+// The array in which all the snippets are been stored, using this array we would be generating the markdown table in SNIPPETS.md file
 let finalSnippets = [["Prefix", "Description"]];
 
 fs.readdir(constants["snippetsFolder"], function (err, files) {
@@ -18,10 +19,12 @@ fs.readdir(constants["snippetsFolder"], function (err, files) {
         const snippetArray = Object.keys(actualData).map(
           key => actualData[key]
         );
+
         // Adding the snippet to the final array
         snippetArray.map(snippet => {
           finalSnippets.push([`\`${snippet.prefix}\``, snippet.description]);
         });
+
         // Generating a markdown table
         let snippetTable = markdownTable(finalSnippets);
         const snippetStats = `thirdsnips currently has a total of ${finalSnippets.length} snippets.`;
@@ -34,12 +37,14 @@ fs.readdir(constants["snippetsFolder"], function (err, files) {
           snippetTable +
           "\n" +
           readmeBottom;
+
         // Clearing all the content in the README.md file before adding new content
         fs.writeFile(constants["readmeFile"], "", function (err) {
           if (err) {
             console.log(err);
           }
         });
+
         // Adding the new content to the README.md file
         fs.writeFile(constants["readmeFile"], snippetDocs, function (err) {
           if (err) {
--- src/automation/scripts/generateSnippets.js
+++ src/automation/scripts/generateSnippets.js
@@ -11,29 +11,40 @@ const {
 } = require("../data/constants");
 const renderSnippet = require("../utils/renderSnippet");
 
+// Fetching the snippets from the thirdweb's snippet.json file using GitHub API
 axios.get(snippetsApiUrl).then(response => {
+  // Clearing the snippets folder before adding new snippets
   fs.writeFile(`${snippetsFolder}/${snippetsFile}`, "", function (err) {
     if (err) {
       console.log(err);
     }
   });
   try {
+    // Adding the initial "{" to the snippets file
     fs.appendFileSync(`${snippetsFolder}/${snippetsFile}`, "{");
   } catch {
+    // If the snippets folder doesn't exist then create one and then add the initial "{" to the snippets file
     fs.mkdirSync(snippetsFolder);
     fs.appendFileSync(`${snippetsFolder}/${snippetsFile}`, "{");
   }
+
   for (let contract in response.data) {
+    // Accessing the methods array of each contract from the response data
     objectPath.get(response.data, `${contract}.methods`).map(method => {
+      // Converting into the form which VSCode supports for snippets
       const snippet = renderSnippet(
         objectPath.get(method, "examples.javascript"),
         method["name"],
         method["summary"].replace(/\n/g, "")
       );
+      // Adding the snippet to the snippets file
       fs.appendFileSync(`${snippetsFolder}/${snippetsFile}`, `${snippet}`);
     });
   }
+  // Removing the last "," from the snippets file
   shelljs.exec(`truncate -s -1 ${snippetsFilePath}`);
+  // Adding the final "}" to the snippets file
   fs.appendFileSync(`${snippetsFolder}/${snippetsFile}`, "}");
+  // Formatting the file
   shelljs.exec("yarn format");
 });
