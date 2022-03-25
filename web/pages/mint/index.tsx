78572f625046808444daee9142318490ca1cb2ef
Anurag <kr.anurag24@gmail.com>
2022-03-25 18:41:24 +0530

    ✨ feat: add react and sdk snippets (#17)

--- web/pages/mint/index.tsx
+++ web/pages/mint/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import { Box, Text, Button, Input } from "@chakra-ui/react";
 
