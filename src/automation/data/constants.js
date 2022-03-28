ca41028afbb00221ff8c05e43da9d447b04fbfde
Kira <90365542+Kira272921@users.noreply.github.com>
2022-03-28 21:16:14 +0530

    :sparkles: feat: generate snippets.md in extension dir (#27)

--- src/automation/data/constants.js
+++ src/automation/data/constants.js
@@ -4,7 +4,7 @@ const constants = {
   snippetsFilePath: "src/extension/snippets/snippets.json",
   snippetsApiUrl:
     "https://raw.githubusercontent.com/thirdweb-dev/typescript-sdk/main/docs/snippets.json",
-  readmeFile: "SNIPPETS.md",
+  readmeFile: ["SNIPPETS.md", "src/extension/SNIPPETS.md"],
 };
 
 module.exports = constants;
--- src/automation/scripts/generateReadme.js
+++ src/automation/scripts/generateReadme.js
@@ -2,60 +2,54 @@ const fs = require("fs");
 const markdownTable = require("markdown-table");
 
 const { readmeTop, readmeBottom } = require("../data/readmeConstants");
-const constants = require("../data/constants");
+const { snippetsFolder, readmeFile } = require("../data/constants");
 
 // The array in which all the snippets are been stored, using this array we would be generating the markdown table in SNIPPETS.md file
 let finalSnippets = [["Prefix", "Description", "Category"]];
 
-fs.readdir(constants["snippetsFolder"], function (err, files) {
+fs.readdir(snippetsFolder, function (err, files) {
   files.map(file => {
     // Reading the files in the snippet folder and getting the JSON file
-    fs.readFile(
-      `${constants["snippetsFolder"]}/${file}`,
-      "utf8",
-      function (err, data) {
-        const actualData = JSON.parse(data);
-        // Converting the object to an array
-        const snippetArray = Object.keys(actualData).map(
-          key => actualData[key]
-        );
+    fs.readFile(`${snippetsFolder}/${file}`, "utf8", function (err, data) {
+      const actualData = JSON.parse(data);
+      // Converting the object to an array
+      const snippetArray = Object.keys(actualData).map(key => actualData[key]);
 
-        // Adding the snippet to the final array
-        snippetArray.map(snippet => {
-          finalSnippets.push([
-            `\`${snippet.prefix}\``,
-            snippet.description.split("|")[0],
-            snippet.description.split("|")[1],
-          ]);
-        });
+      // Adding the snippet to the final array
+      snippetArray.map(snippet => {
+        finalSnippets.push([
+          `\`${snippet.prefix}\``,
+          snippet.description.split("|")[0],
+          snippet.description.split("|")[1],
+        ]);
+      });
 
-        // Generating a markdown table
-        let snippetTable = markdownTable(finalSnippets);
-        const snippetStats = `thirdsnips currently has a total of ${finalSnippets.length} snippets.`;
+      // Generating a markdown table
+      let snippetTable = markdownTable(finalSnippets);
+      const snippetStats = `thirdsnips currently has a total of ${finalSnippets.length} snippets.`;
 
-        const snippetDocs =
-          readmeTop +
-          "\n" +
-          snippetStats +
-          "\n\n" +
-          snippetTable +
-          "\n" +
-          readmeBottom;
+      const snippetDocs =
+        readmeTop +
+        "\n" +
+        snippetStats +
+        "\n\n" +
+        snippetTable +
+        "\n" +
+        readmeBottom;
 
-        // Clearing all the content in the README.md file before adding new content
-        fs.writeFile(constants["readmeFile"], "", function (err) {
+      readmeFile.forEach(snippetDocFile => {
+        fs.writeFile(snippetDocFile, "", function (err) {
           if (err) {
             console.log(err);
           }
         });
 
-        // Adding the new content to the README.md file
-        fs.writeFile(constants["readmeFile"], snippetDocs, function (err) {
+        fs.writeFile(snippetDocFile, snippetDocs, function (err) {
           if (err) {
             console.log(err);
           }
         });
-      }
-    );
+      });
+    });
   });
 });
